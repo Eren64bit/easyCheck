098fe606765f51557e45ee4b1228628eba02c152
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-05-20 12:33:12 +0300

    Version 0.01

---
+++ easyCheck.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+#define MAX_LIMIT 40
+#define BLANKSPACE -1
+#define NORMAL 0
+#define IN_STRING 1
+#define IN_CHAR 2
+#define IN_COMMENT 3
+#define IN_LINE_COMMENT 4
+/*exercise 1-24. Write a program to check a C program for rudimentary syntax errors like
+unmatched parentheses, brackets and braces. Don't forget about quotes, both single and
+double, escape sequences, and comments. (This program is hard if you do it in full generality.)*/
+
+//char size = 1 byte
+
+
+
+int stateMachine(char current, int prev, int state){
+
+    if (state == NORMAL) {
+        if (prev == '/' && current == '/') return IN_LINE_COMMENT;
+        if (prev == '/' && current == '*') return IN_COMMENT;
+        if (current == '\"') return IN_STRING;
+        if (current == '\'') return IN_CHAR;
+    }
+    return state;
+}
+
+
+FILE* openFile(char filePath[]){
+    FILE* fptr = fopen(filePath, "r");
+
+    if (fptr == NULL){
+        printf("File openning failed");
+        return NULL;
+    }
+
+    return fptr;
+}
+
+void readFile(FILE* ptr){
+    int c, prev;
+    int state = NORMAL;
+
+    while ((c = fgetc(ptr)) != EOF) {
+        state = stateMachine(c, prev, state);
+        prev = c;
+    }
+}
+
+void handleFile(){
+
+    char str[MAX_LIMIT];
+    printf("Please enter file path which you want to scan\n");
+
+    fgets(str, MAX_LIMIT, stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    FILE *ptr = openFile(str);
+
+    readFile(ptr);
+
+}
+
+int main(){
+
+    handleFile();
+    return 0;
+}
+
